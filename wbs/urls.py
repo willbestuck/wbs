26b50f3779ac65da665fd278b7691258418dbb5b
Ben Stueck <wbstueck@gmail.com>
2014-10-12 10:04:31 -0700

    Adding django project wbs

--- wbs/urls.py
+++ wbs/urls.py
@@ -1,10 +1,10 @@
 from django.conf.urls import patterns, include, url
 from django.contrib import admin
+from django.views.generic.base import RedirectView
 
 urlpatterns = patterns('',
     # Examples:
-    # url(r'^$', 'wbs.views.home', name='home'),
-    # url(r'^blog/', include('blog.urls')),
-
+    url(r'^$', RedirectView.as_view(url='info/', permanent=False)),
+    url(r'^info/', include('info.urls')),
     url(r'^admin/', include(admin.site.urls)),
 )
