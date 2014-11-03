788750457c5120b35633a9eb154aaeedac1948c0
ben <wbstueck@gmail.com>
2014-11-03 14:55:01 -0800

    added urls and templates for navigation

--- info/urls.py
+++ info/urls.py
@@ -2,6 +2,9 @@ from django.conf.urls import patterns, url
 
 from info import views
 
-urlpatterns = patterns('',
-	url(r'^$', views.index, name='index'),
-)
+# urlpatterns = patterns('',
+# 	url(r'^$', views.index, name='index'),
+# 	url(r'^about/', views.about, name='about'),
+# 	url(r'^resume/', views.resume, name='resume'),
+# 	url(r'^contact/', views.contact, name='contact'),
+# )
