b83279fbd82ab2358efc47e6db7a1392ed7901f4
Ben Stueck <wbstueck@gmail.com>
2015-01-11 15:53:20 -0800

    Fixed secret key for production

--- wbs/settings.py
+++ wbs/settings.py
@@ -17,7 +17,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 # See https://docs.djangoproject.com/en/1.7/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '5b=)np=-!-xand_exb5q*bw@(vf)vll$=tn&8#uw7wolus2v1b'
+SECRET_KEY = os.environ['SECRET_KEY']
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
